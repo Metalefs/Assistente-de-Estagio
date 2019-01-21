2f702d59db44f7aad96314c11e20c7899e4693e7
Metalefs <40893204+Metalefs@users.noreply.github.com>
2019-01-21 16:07:21 -0200

    Começo do desenvolvimento da pagina de seleção de documentos
    Adicionado um cartão para cada documento
    
    Trabalho na função de mostrar o documento pelo id
    
    Correção da exibição da pagina de preenchimento
    
    **Migração do banco de dados para servidor remoto em andamento

---
+++ Models/RequisitoDeUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assistente_de_Estagio.Models
+{
+    public partial class Requisitodeusuario
+    {
+        public int? UsuarioIdUsuario { get; set; }
+        public int? RequisitosIdRequisito { get; set; }
+        public string Dados { get; set; }
+        public int IdRequisistoDeUsuario { get; set; }
+    }
+}
